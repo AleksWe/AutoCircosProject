5ad0491f124dcfe665f8f1f85e09420c0635511f
AleksWe <aleksandra.weiss28@gmail.com>
2024-07-28 22:12:47 +0200

    small changes in fast_api_starter.py + index.html with checkbox startup

--- fast_api_starter.py
+++ fast_api_starter.py
@@ -1,13 +1,22 @@
 import uvicorn
 from typing import List
 from fastapi.responses import HTMLResponse
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, Form, File, UploadFile, Request
+from fastapi.templating import Jinja2Templates
 import subprocess
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
 
 @app.post("/upload")
-def upload(files: List[UploadFile] = File(...)):
+def upload(request: Request,
+           option: str = Form(...),
+           files: List[UploadFile] = File(...)):
+    # For now, we will just print the option and the file name
+    print(f"Selected Option: {option}")
+    print(f"Uploaded Files: {[file.filename for file in files]}")
+
     for file in files:
         try:
             contents = file.file.read()
@@ -18,23 +27,26 @@ def upload(files: List[UploadFile] = File(...)):
         finally:
             file.file.close()
     subprocess.run('./circos_project.sh', shell=True)
-    return {"message": f"Successfuly uploaded {[file.filename for file in files]}. Circos generator proceeding..."}
-    # Bash file
+    return {"message": f"Successfuly uploaded. Circos generator proceeding..."}
 
 
 # Access the form at 'http://0.0.0.0:8000/' from your browser
-@app.get('/')
-async def main():
-    content = '''
-    <body>
-    <form action='/upload' enctype='multipart/form-data' method='post'>
-    <input name='files' type='file' multiple>
-    <input type='submit'>
-    </form>
-    </body>
-    '''
-    return HTMLResponse(content=content)
+@app.get("/", response_class=HTMLResponse)
+def get_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
+#@app.get('/')
+#async def main():
+#    content = '''
+#    <body>
+#    <form action='/upload' enctype='multipart/form-data' method='post'>
+#    <input name='files' type='file' multiple>
+#    <input type='submit'>
+#    </form>
+#    </body>
+#    '''
+#    return HTMLResponse(content=content)
 
+# Only for testing
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
